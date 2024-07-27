163b34a03b7bad0514fdf15ec04fc4e1010e0056
William Marrujo <willmarrujo@gmail.com>
2024-07-27 16:21:24 -0500

    update recommendations

--- src/routes/recommendations/+page.svelte
+++ src/routes/recommendations/+page.svelte
@@ -18,16 +18,94 @@
 		<article class="prose lg:prose-xl prose-invert shadow-lg p-10">
 			<h1 class="h1 text-center">Recommendations</h1>
 			<ul class="text-2xl">
-				<li><a href="https://docs.google.com/spreadsheets/d/1KRnU3sqC9SOir41nX-W9bFKeoXA2moFyOQl5owUbIY0/edit?usp=sharing">Book List</a></li>
-				<li><a href="https://medium.com/@bre/the-cult-of-done-manifesto-724ca1c2ff13">The Cult of Done</a></li>
-				<li>Mind-Bending Games
+				<li>Manifestos
+					<ul>
+						<li><a href="https://agilemanifesto.org/">Agile</a> (<a href="https://agilemanifesto.org/principles.html">12 principles</a>)</li>
+						<li><a href="https://medium.com/@bre/the-cult-of-done-manifesto-724ca1c2ff13">The Cult of Done</a></li>
+						<li><a href="https://content.chrisguillebeau.com/wp-content/uploads/2023/05/worlddomination.pdf">A Brief Guide to World Domination</a></li>
+					</ul>
+				</li>
+				<li><a href="https://docs.google.com/spreadsheets/d/1KRnU3sqC9SOir41nX-W9bFKeoXA2moFyOQl5owUbIY0/edit?usp=sharing">Books</a></li>
+				<li>Games
 					<ul>
 						<li><a href="https://store.steampowered.com/app/2147950/4D_Golf/">4D Golf</a></li>
-						<li><a href="https://store.steampowered.com/app/1349230/5D_Chess_With_Multiverse_Time_Travel/">5D Chess with Multiverse Time Travel</a></li>
-						<li><a href="https://store.steampowered.com/app/219890/Antichamber/">Antichamber</a></li>
 						<li><a href="https://store.steampowered.com/app/224760/FEZ/">Fez</a></li>
+						<li><a href="https://store.steampowered.com/app/219890/Antichamber/">Antichamber</a></li>
+						<li><a href="https://boardgamegeek.com/boardgame/171131/captain-sonar">Captain Sonar</a></li>
 						<li><a href="https://store.steampowered.com/app/1256230/Hyperbolica/">Hyperbolica</a></li>
 						<li><a href="https://store.steampowered.com/app/1049410/Superliminal/">Superliminal</a></li>
+						<li><a href="https://store.steampowered.com/app/1349230/5D_Chess_With_Multiverse_Time_Travel/">5D Chess</a></li>
+					</ul>
+				</li>
+				<li>Youtube Channels
+					<ul>
+						<li>Math & Science
+							<ul>
+								<li><a href="https://www.youtube.com/@3blue1brown">3Blue1Brown</a></li>
+								<li><a href="https://www.youtube.com/@AlphaPhoenixChannel">AlphaPhoenix Channel</a></li>
+								<li><a href="https://www.youtube.com/@braincraft">BrainCraft</a></li>
+								<li><a href="https://www.youtube.com/@CGPGrey">CGPGrey</a></li>
+								<li><a href="https://www.youtube.com/@GeographyNow">Geography Now</a></li>
+								<li><a href="https://www.youtube.com/@JayForeman">Jay Foreman</a></li>
+								<li><a href="https://www.youtube.com/@journeytomicro">Journey to the Microcosmos</a></li>
+								<li><a href="https://www.youtube.com/@kurzgesagt">Kurzgesagt</a></li>
+								<li><a href="https://www.youtube.com/@LookingGlassUniverse">Looking Glass Universe</a></li>
+								<li><a href="https://www.youtube.com/@MarkRober">Mark Rober</a></li>
+								<li><a href="https://www.youtube.com/@MinuteEarth">Minute Earth</a></li>
+								<li><a href="https://www.youtube.com/@MinutePhysics">Minute Physics</a></li>
+								<li><a href="https://www.youtube.com/@pbsinfiniteseries">PBS Infinite Series</a></li>
+								<li><a href="https://www.youtube.com/@pbsideachannel">PBS Idea Channel</a></li>
+								<li><a href="https://www.youtube.com/@pbsspacetime">PBS SpaceTime</a></li>
+								<li><a href="https://www.youtube.com/@PracticalEngineeringChannel">Practical Engineering</a></li>
+								<li><a href="https://www.youtube.com/@PrimerBlobs">Primer</a></li>
+								<li><a href="https://www.youtube.com/@RealEngineering">Real Engineering</a></li>
+								<li><a href="https://www.youtube.com/@Reducible">Reducible</a></li>
+								<li><a href="https://www.youtube.com/@scottmanley">Scott Manley</a></li>
+								<li><a href="https://www.youtube.com/@scishowspace">Scishow Space</a></li>
+								<li><a href="https://www.youtube.com/@smartereveryday">Smarter Every Day</a></li>
+								<li><a href="https://www.youtube.com/@standupmaths">Stand-Up Maths</a></li>
+								<li><a href="https://www.youtube.com/@SteveMould">Steve Mould</a></li>
+								<li><a href="https://www.youtube.com/@TheBackyardScientist">The Backyard Scientist</a></li>
+								<li><a href="https://www.youtube.com/@ThisPlaceChannel">This Place</a></li>
+								<li><a href="https://www.youtube.com/@TierZoo">Tier Zoo</a></li>
+								<li><a href="https://www.youtube.com/@veritasium">Veritasium</a></li>
+								<li><a href="https://www.youtube.com/@Vihart">Vihart</a></li>
+								<li><a href="https://www.youtube.com/@zachstar">Zach Star</a></li>
+								<li><a href="https://www.youtube.com/@ZoggFromBetelgeuse">ZoggFromBetelgeuse</a></li>
+							</ul>
+						</li>
+						<li>Design & Music
+							<ul>
+								<li><a href="https://www.youtube.com/@AltoClef">Alto Clef</a></li>
+								<li><a href="https://www.youtube.com/@razbuten">Razbuten</a></li>
+								<li><a href="https://www.youtube.com/@extracredits">Extra Credits</a></li>
+								<li><a href="https://www.youtube.com/@Sideways440">Sideways</a></li>
+								<li><a href="https://www.youtube.com/@Tantacrul">Tantacrul</a></li>
+								<li><a href="https://www.youtube.com/@twosetviolin">Twoset Violin</a></li>
+							</ul>
+						</li>
+						<li>Lifestyle & Philosophy
+							<ul>
+								<li><a href="https://www.youtube.com/@answerinprogress">Answer in Progress</a></li>
+								<li><a href="https://www.youtube.com/@PhilosophyTube">PhilosophyTube</a></li>
+								<li><a href="https://www.youtube.com/@TomScottGo">Tom Scott</a></li>
+								<li><a href="https://www.youtube.com/@vlogbrothers">Vlogbrothers</a></li>
+								<li><a href="https://www.youtube.com/@Vsauce">Vsauce</a></li>
+								<li><a href="https://www.youtube.com/@wheezywaiter">Wheezy Waiter</a></li>
+							</ul>
+						</li>
+						<li>History & Politics & Language
+							<ul>
+								<li><a href="https://www.youtube.com/@ClimateTown">ClimateTown</a></li>
+								<li><a href="https://www.youtube.com/@extrahistory">Extra History</a></li>
+								<li><a href="https://www.youtube.com/@Kraut_the_Parrot">Kraut</a></li>
+								<li><a href="https://www.youtube.com/@NotJustBikes">Not Just Bikes</a></li>
+								<li><a href="https://www.youtube.com/@RareEarthSeries">Rare Earth</a></li>
+								<li><a href="https://www.youtube.com/@RationalAnimations">Rational Animations</a></li>
+								<li><a href="https://www.youtube.com/@NativLang">NativLang</a></li>
+								<li><a href="https://www.youtube.com/@Xidnaf">Xidnaf</a></li>
+							</ul>
+						</li>
 					</ul>
 				</li>
 			</ul>
